d4bbf316247b971132323d1a7a4d51f8a5fd883d
ehddn <ehddn2202@gmail.com>
2023-11-04 02:51:21 +0900

    Bootstrap 적용중 - main.html

--- maple_diary/settings.py
+++ maple_diary/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
     'blog',
     'user',
 ]
@@ -119,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # MEDIA_ROOT = BASE_DIR / 'media'
